fe695eb0195b6e04a33fc454a2d7b1b13e954759
Shaksham Agarwal <57569810+sam31072000@users.noreply.github.com>
2021-06-15 19:27:47 +0530

    Update

--- stockproject/celery.py
+++ stockproject/celery.py
@@ -14,11 +14,7 @@ app.conf.update(timezone = 'Asia/Kolkata')
 app.config_from_object(settings, namespace='CELERY')
 
 app.conf.beat_schedule = {
-    # 'every-10-seconds' : {
-    #     'task': 'mainapp.tasks.update_stock',
-    #     'schedule': 10,
-    #     'args': (['RELIANCE.NS', 'BAJAJFINSV.NS'],)
-    # },
+    
 }
 
 app.autodiscover_tasks()
