66078624159b7d125e74d3ff1895b0973c9a5eb3
Shaksham Agarwal <57569810+sam31072000@users.noreply.github.com>
2021-09-01 01:10:03 +0530

    Update celery.py

--- stockproject/celery.py
+++ stockproject/celery.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 import os
 
+#django beat celery 
+
 from celery import Celery
 from django.conf import settings
 #from celery.schedules import crontab
@@ -14,7 +16,9 @@ app.conf.update(timezone = 'Asia/Kolkata')
 app.config_from_object(settings, namespace='CELERY')
 
 app.conf.beat_schedule = {
-    
+    'every-10-seconds':{   #to req every 10 secs
+        'task': 'm'
+    }
 }
 
 app.autodiscover_tasks()
