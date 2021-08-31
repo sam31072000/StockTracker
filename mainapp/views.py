8a7cba79b0e635915eb0816a456be11463aedaf6
Shaksham Agarwal <57569810+sam31072000@users.noreply.github.com>
2021-09-01 00:44:22 +0530

    Update views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,59 +1,29 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from yahoo_fin.stock_info import *
-import time
-import queue
-from threading import Thread
-from asgiref.sync import sync_to_async
 # Create your views here.
 def stockPicker(request):
-    stock_picker = tickers_nifty50()
+    stock_picker = tickers_nifty50()  #to fetch stocks present in nifty
     print(stock_picker)
-    return render(request, 'mainapp/stockpicker.html', {'stockpicker':stock_picker})
+    return render(request, 'mainapp/stockpicker.html', {'stockpicker':stock_picker}) #sending list of available stocks to frontend
 
-@sync_to_async
-def checkAuthenticated(request):
-    if not request.user.is_authenticated:
-        return False
-    else:
-        return True
-
-async def stockTracker(request):
+def stockTracker(request):
     is_loginned = await checkAuthenticated(request)
     if not is_loginned:
         return HttpResponse("Login First")
-    stockpicker = request.GET.getlist('stockpicker')
+    stockpicker = request.GET.getlist('stockpicker') #to get list of selected stocks
     print(stockpicker)
     data = {}
-    available_stocks = tickers_nifty50()
+    available_stocks = tickers_nifty50() #avalable stocks in nifty
     for i in stockpicker:
         if i in available_stocks:
             pass
         else:
             return HttpResponse("Error")
     
-    n_threads = len(stockpicker)
-    thread_list = []
-    que = queue.Queue()
-    start = time.time()
-    # for i in stockpicker:
-    #     result = get_quote_table(i)
-    #     data.update({i: result})
-    for i in range(n_threads):
-        thread = Thread(target = lambda q, arg1: q.put({stockpicker[i]: get_quote_table(arg1)}), args = (que, stockpicker[i]))
-        thread_list.append(thread)
-        thread_list[i].start()
-
-    for thread in thread_list:
-        thread.join()
-
-    while not que.empty():
-        result = que.get()
-        data.update(result)
-    end = time.time()
-    time_taken =  end - start
-    print(time_taken)
-            
+    for i in stockpicker:
+        result = get_quote_table(i)  #this function used to request data of each stock from yahoo fin library
+        data.update({i: result})
     
     print(data)
     return render(request, 'mainapp/stocktracker.html', {'data': data, 'room_name': 'track'})
